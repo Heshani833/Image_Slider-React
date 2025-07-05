9f4b8752f29b4f0e9e5bf2bdcc6facf8fb3d1451
Heshani833 <heshanishehana123@gmail.com>
2025-07-05 16:42:28 +0530

    Image Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
-import BasicSlider from "./components/BasicSlider/BasicSlider.jsx";
+import { act, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import { data } from "./data";
+
+const App = () => {
+  const [activeImg, setActiveImg] = useState(0);
+  const handlePrev = () => {
+    setActiveImg((activeImg - 1 + data.length) % data.length);
+  };
+
+  const handleNext = () => {
+    setActiveImg((activeImg + 1) % data.length);
+  };
 
   return (
     <>
-      <BasicSlider />
+      <div className="carousel">
+        <button onClick={handlePrev}>Preview</button>
+        {data.map((item, i) => {
+          return (
+            <img
+              className={activeImg === i ? "img show" : "img hide"}
+              src={item.url}
+              alt={item.alt}
+              key={item.id}
+              title={item.alt}
+            />
+          );
+        })}
+        <button onClick={handleNext}>Next</button>
+      </div>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,27 @@
+import flowerImg from "./assets/flower.png";
+import kittenImg from "./assets/kitten.png";
+import natureImg from "./assets/nature.png";
+import foxImg from "./assets/fox.png";
+
+export const data = [
+  {
+    id: 1,
+    url: flowerImg,
+    alt: "Flower",
+  },
+  {
+    id: 2,
+    url: kittenImg,
+    alt: "Kitten",
+  },
+  {
+    id: 3,
+    url: natureImg,
+    alt: "Nature",
+  },
+  {
+    id: 4,
+    url: foxImg,
+    alt: "Fox",
+  },
+];
